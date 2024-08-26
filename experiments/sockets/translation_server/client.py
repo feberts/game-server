c695271548d9875817330b7c3ae234bbeeb0e33a
feberts <64541491+feberts@users.noreply.github.com>
2024-08-26 19:11:46 +0200

    Socket tests

---
+++ experiments/sockets/translation_server/client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import socket
+
+IP = '127.0.0.1'
+PORT = 4711
+BUFFER_SIZE = 1024
+
+print(f"Connecting to {IP}:{PORT} ...")
+sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sd.connect((IP, PORT))
+print('connected')
+
+while True:
+    write = input('Enter a word: ')
+    print(f"Sending data: {write}")
+    write = bytes(write, 'utf-8')
+    sd.sendall(write)
+
+    read = sd.recv(BUFFER_SIZE)
+    print(f"=> {str(read, 'utf-8').strip()}")
+
+sd.close() # never executed

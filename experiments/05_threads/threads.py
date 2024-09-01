1bbb21a051d76e5d6a8ce845ebc504d90f7529cf
feberts <64541491+feberts@users.noreply.github.com>
2024-09-01 15:18:35 +0200

    Daemon threads

--- experiments/05_threads/threads.py
+++ experiments/05_threads/threads.py
@@ -4,7 +4,6 @@
 import threading
 import time
 
-
 def thread_function(arg):
     for _ in range(5):
         print(arg)
@@ -12,16 +11,21 @@ def thread_function(arg):
 
 print('Main programm starts')
 
-t1 = threading.Thread(target=thread_function, args=('Thread 1',))
-t2 = threading.Thread(target=thread_function, args=('Thread 2',))
+t1 = threading.Thread(target=thread_function, args=('Thread 1',)) # args as list
+t2 = threading.Thread(target=thread_function, args=('Thread 2',)) # daemon=True for daemon thread
 
+print('Starting thread 1')
 t1.start()
-print('Thread 1 was started')
 
+print('Starting thread 2')
 t2.start()
-print('Thread 2 was started')
 
+print('Waiting for threads to finish')
 t1.join()
 t2.join()
 
 print('Main programm ends')
+
+# - join() waits for both regular and daemon threads
+# - daemon threads (daemon=True) are killed when the program ends or is killed
+# - regular threads continue execution after the main program has ended

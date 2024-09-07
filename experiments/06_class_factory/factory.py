89d2f4cd14864e71ad89738607033f084b8c3fc3
feberts <64541491+feberts@users.noreply.github.com>
2024-09-07 20:38:06 +0200

    Documenting factory implementation

--- experiments/06_class_factory/factory.py
+++ experiments/06_class_factory/factory.py
@@ -2,6 +2,8 @@
 
 # implementing a method that creates objects by class name
 
+# Each time a client request the start of a new game, the server must create an object of the corresponding game class. It makes sense to have a mechanism that allows class instantiation by class name, since the clients are not aware of the game classes on the server and can only provide the game name as a string.
+
 class Base:
     def override_me(self):
         raise NotImplementedError
@@ -21,6 +23,7 @@ for cl in classes:
     classes_by_name[type(cl()).__name__] = cl
 
 def instantiate(name):
+    """factory method"""
     return classes_by_name[name]()
 
 cl = instantiate('DerivedA')

3bb32bb2df6d8dad47315cbed4e56b28f1d0efbe
feberts <64541491+feberts@users.noreply.github.com>
2024-10-15 20:06:17 +0200

    Experimenting with generic api

--- experiments/07_generic_api_function/generic_move.py
+++ experiments/07_generic_api_function/generic_move.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# The goal is to reduce the amount of code required for the client API. Instead of providing a separate move-function for each game, a generic function could be used, that accepts the arguments as keyword args. The move could then be sent to the server as a dictionary/json.
+# The goal is to reduce the amount of code required for the client API. Instead of providing a separate move-function for each game (or even multiple functions for a single game), a generic function could be used, that accepts the arguments as keyword args. The move could then be sent to the server as a JSON-object/dictionary.
 
 import json
 
@@ -23,22 +23,24 @@ class Api:
         print('json:', j)
 
 api = Api()
-
 api.move(piece='pawn', x='d', y=7) # pawn to D7
-
 
 ##### SERVER #####
 
-class Game:
-    def move(self, piece, x, y):
-        print('Game.move()')
-        print(f'piece = {piece}, x = {x}, y = {y}')
+class Server:
+    class Game:
+        def move(self, args):
+            piece, x, y, = args['piece'], args['x'], args['y']
+            print('Game.move()')
+            print(f'piece = {piece}, x = {x}, y = {y}')
 
-def receive_move():
-    print('receive_move()')
-    d = json.loads(data)
-    print('dict: ', d)
-    game.move(d['piece'], d['x'], d['y'])
+    game = Game()
+    
+    def receive_move(self):
+        print('receive_move()')
+        d = json.loads(data)
+        print('dict: ', d)
+        self.game.move(d)
 
-game = Game()
-receive_move()
+server = Server()
+server.receive_move()
--- experiments/08_dictionary_verification/dictionary_check.py
+++ experiments/08_dictionary_verification/dictionary_check.py
@@ -7,19 +7,19 @@ The idea is, to simplify verification of dictionaries passed to a function. Befo
 - does the key you want to access exist?
 - has its value the expected data type?
 
-One could omit all checks and access the dictionary in a try-except-block and then perform an appropriate action, but the goal here is, to have a designated function for checking dictionaries. This allows for tidier code.
+One could omit all checks and access the dictionary in a try-except-block and then, if an exception is raised, perform an appropriate action, but the goal here is, to have a designated function for checking dictionaries. This allows for tidier code.
 """
 
 d = {'a':'hallo', 'b':2, 'c':3.12, 'd':True, 'e':[1,2,3], 'f':(1,2), 'g':{'a':1,'b':2}}
 
-expected_kwargs = {'a':str, 'b':int, 'c':float, 'd':bool, 'e':list, 'f':tuple, 'g':dict}
+expected = {'a':str, 'b':int, 'c':float, 'd':bool, 'e':list, 'f':tuple, 'g':dict}
 
 
 if type(d) != dict:
     print('not a valid dictionary')
     exit()
 
-for key_name, val_type in expected_kwargs.items():
+for key_name, val_type in expected.items():
     #print(key_name, val_type)
     if key_name not in d:
         print(f"key '{key_name}' missing in dictionary")

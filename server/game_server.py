94f7d59c60790e97cf4412024fa0f4aad0413258
feberts <64541491+feberts@users.noreply.github.com>
2024-10-19 20:15:48 +0200

    handling requests in seperate thread

--- server/game_server.py
+++ server/game_server.py
@@ -6,10 +6,29 @@ TODO
 import json
 import socket
 import time # TODO weg
+import threading
 
 IP = '127.0.0.1'
 PORT = 4711
 BUFFER_SIZE = 1024
+
+def request_handler(conn):
+    with conn:
+        # receive data from client:
+        read = conn.recv(BUFFER_SIZE)
+        read = str(read, 'utf-8')
+        read = json.loads(read)
+        print(f"Received data from {ip}:{port}: {read}")
+
+        # send data to client:
+        reply = {'Vom Sever':456}
+        state = json.dumps(reply)
+        conn.sendall(bytes(state, 'utf-8'))
+
+        # close connection:
+        #conn.close() # TODO weg
+        #time.sleep(5) # TODO weg
+        print(f"Closed connection to {ip}:{port}")
 
 try:
     # open listening socket:
@@ -25,22 +44,25 @@ try:
             ip, port = client
             print(f'Accepted connection from {ip}:{port}')
 
-            with conn:
-                # receive data from client:
-                read = conn.recv(BUFFER_SIZE)
-                read = str(read, 'utf-8')
-                read = json.loads(read)
-                print(f"Received data from {ip}:{port}: {read}")
+            t = threading.Thread(target=request_handler, args=(conn,), daemon=True)
+            t.start()
+            #request_handler(conn)
+            #with conn:
+                ## receive data from client:
+                #read = conn.recv(BUFFER_SIZE)
+                #read = str(read, 'utf-8')
+                #read = json.loads(read)
+                #print(f"Received data from {ip}:{port}: {read}")
 
-                # send data to client:
-                reply = {'Vom Sever':456}
-                state = json.dumps(reply)
-                conn.sendall(bytes(state, 'utf-8'))
-                #time.sleep(1000) # TODO weg
+                ## send data to client:
+                #reply = {'Vom Sever':456}
+                #state = json.dumps(reply)
+                #conn.sendall(bytes(state, 'utf-8'))
+                ##time.sleep(1000) # TODO weg
 
-                # close connection:
-                #conn.close() # TODO weg
-                print(f"Closed connection to {ip}:{port}")
+                ## close connection:
+                ##conn.close() # TODO weg
+                #print(f"Closed connection to {ip}:{port}")
 
 except KeyboardInterrupt:
     pass

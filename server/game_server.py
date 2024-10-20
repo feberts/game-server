1d058faaf6fc309e3057ff7a39a2ab997d8f515f
feberts <64541491+feberts@users.noreply.github.com>
2024-10-21 00:53:33 +0200

    Limiting size of data t received

--- server/game_server.py
+++ server/game_server.py
@@ -27,8 +27,8 @@ def request_handler(conn):
             data = conn.recv(4096)
             if not data: raise ClientDisconnect
             request += data
+            if len(request) > 1e6: raise DataSizeExceeded
             if request[-5:] == b'_EOF_': break
-            if len(request) > 1000: raise DataSizeExceeded
         
         # prepare data:
         request = request[:-5] # strip EOF

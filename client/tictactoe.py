a0431a5f48258e32e5ab818adc05d8c0ff0c8606
feberts <64541491+feberts@users.noreply.github.com>
2024-10-21 14:45:45 +0200

    Test cases for start_game()

--- client/tictactoe.py
+++ client/tictactoe.py
@@ -11,6 +11,31 @@ api = GameServerAPI()
 
 player_id, msg = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', players=2, token='mygame')
 
+#player_id, msg = api.start_game(server='127.0.0.11', port=4711, game='TicTacToe', players=2, token='mygame')
+#player_id, msg = api.start_game(server='127.0.0.256', port=4711, game='TicTacToe', players=2, token='mygame')
+#player_id, msg = api.start_game(server='sdfgh', port=4711, game='TicTacToe', players=2, token='mygame')
+#player_id, msg = api.start_game(server='', port=4711, game='TicTacToe', players=2, token='mygame')
+#player_id, msg = api.start_game(server=34, port=4711, game='TicTacToe', players=2, token='mygame')
+#player_id, msg = api.start_game(server='', port=4712, game='TicTacToe', players=2, token='mygame')
+
+
+#player_id, msg = api.start_game(server='127.0.0.1', port=4712, game='TicTacToe', players=2, token='mygame')
+#player_id, msg = api.start_game(server='127.0.0.1', port=471159875, game='TicTacToe', players=2, token='mygame')
+#player_id, msg = api.start_game(server='127.0.0.1', port="4711", game='TicTacToe', players=2, token='mygame')
+#player_id, msg = api.start_game(server='127.0.0.1', port=-4711, game='TicTacToe', players=2, token='mygame')
+
+
+#player_id, msg = api.start_game(server='127.0.0.1', port=4711, game='', players=2, token='mygame')
+#player_id, msg = api.start_game(server='127.0.0.1', port=4711, game=456, players=2, token='mygame')
+
+
+#player_id, msg = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', players=0, token='mygame')
+#player_id, msg = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', players="2", token='mygame')
+
+
+#player_id, msg = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', players=2, token='')
+#player_id, msg = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', players=2, token=8765)
+
 if not player_id:
     print(msg)
     print('end')

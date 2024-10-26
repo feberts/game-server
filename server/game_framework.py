f8a8f1c0fe845e93a468798802c1e4043997b358
feberts <64541491+feberts@users.noreply.github.com>
2024-10-26 19:43:01 +0200

    Add class GameFramework

---
+++ server/game_framework.py
@@ -0,0 +1,20 @@
+"""
+Game framework.
+
+This module implements the game framework. The framework is responsible for managing active games. It serves as an intermediary between the server and active game instances.
+
+Client requests are parsed and the appropriate actions are performed. The following list shows the types of requests the framework can handle and the resulting actions:
+
+- start: instantiate game class objects
+- join: assign clients to games
+- move: forward player moves to game instances
+- state: report the game state to clients
+
+To perform these actions, the framework calls the corresponding methods of a game class, if necessary.
+"""
+
+class GameFramework:
+    """
+    TODO
+    """
+    pass

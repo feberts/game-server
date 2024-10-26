947242471a4195d076868103e0dc087f2029d36b
feberts <64541491+feberts@users.noreply.github.com>
2024-10-26 13:07:39 +0200

    Change return type of function check_dict

--- server/utility.py
+++ server/utility.py
@@ -30,6 +30,7 @@ def server_error(message):
     return generic_error('server', message)
 
 def check_dict(d, expected):
+    # TODO del if not needed
     """
     Checking a dictionary's structure.
 
@@ -37,21 +38,22 @@ def check_dict(d, expected):
 
     Example:
     To check, if dictionary d has keys named 'a' and 'b', that are mapped to values of types int and str, a function call might look like this:
-    ok, msg = check_dict(d, {'a':int, 'b':str})
+
+    d = {'a':42, 'b':'forty-two'}
+    err = check_dict(d, {'a':int, 'b':str})
+    if err: print(err)
 
     Parameters:
     d (dict): dictionary to be checked
     expected (dict): contains the expected key names and value data types
 
     Returns:
-    tuple(bool, str):
-        bool: True, if dictionary has the expected structure, else False
-        str: error message, if dictionary is okay, an empty string otherwise
+    str: None, if dictionary has the expected structure, an error message otherwise
     """
     for key_name, val_type in expected.items():
         if key_name not in d:
-            return False, f"keyword argument '{key_name}' of type {val_type} missing"
+            return f"keyword argument '{key_name}' of type {val_type} missing"
         if type(d[key_name]) != val_type:
-            return False, f"type of argument '{key_name}' must be {val_type}"
+            return f"type of argument '{key_name}' must be {val_type}"
 
-    return True, ''
+    return None

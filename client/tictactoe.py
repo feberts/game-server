fbcacead271d5db1d553e7a8f409e2953644c409
feberts <64541491+feberts@users.noreply.github.com>
2024-10-27 17:23:08 +0100

    Adding test bench

--- client/tictactoe.py
+++ client/tictactoe.py
@@ -5,12 +5,14 @@ Tic-tac-toe client.
 This program connects to the game server to play tic-tac-toe against another client.
 """
 
-from game_server_api import GameServerAPI
 import threading
 import time
 
+from game_server_api import GameServerAPI
+
 def client_start():
     api = GameServerAPI()
+
     my_id, err = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', token='mygame', players=2)
 
     if err:
@@ -21,8 +23,8 @@ def client_start():
 
 def client_join():
     time.sleep(0.1)
-
     api = GameServerAPI()
+
     my_id, err = api.join_game(server='127.0.0.1', port=4711, game='TicTacToe', token='mygame')
 
     if err:
@@ -34,9 +36,7 @@ def client_join():
 
 threading.Thread(target=client_start, args=(), daemon=True).start()
 
-threading.Thread(target=client_join, args=(), daemon=True).start()
-threading.Thread(target=client_join, args=(), daemon=True).start()
-threading.Thread(target=client_join, args=(), daemon=True).start()
-threading.Thread(target=client_join, args=(), daemon=True).start()
+for _ in range(4):
+    threading.Thread(target=client_join, args=(), daemon=True).start()
 
-time.sleep(3)
+time.sleep(5)

4cb11ae77cd81be5668e6ed116ec23324ee636d2
feberts <64541491+feberts@users.noreply.github.com>
2024-10-27 10:37:18 +0100

    Multithreaded test for starting and joining games

--- client/tictactoe.py
+++ client/tictactoe.py
@@ -6,15 +6,32 @@ This program connects to the game server to play tic-tac-toe against another cli
 """
 
 from game_server_api import GameServerAPI
+import threading
 
-api = GameServerAPI()
+def client_1():
+    api = GameServerAPI()
 
-my_id, err = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', token='mygame', players=2)
+    my_id, err = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', token='mygame', players=2)
 
-#my_id, err = api.start_game(server='127.0.0.1', port=4711, game='TicTacToe', token='mygame2', players=2) # TODO del
+    if err:
+        print(err)
+        exit()
 
-if err:
-    print(err)
-    exit()
+    print('Player ID:', my_id)
 
-print('Player ID:', my_id)
+def client_2():
+    api = GameServerAPI()
+
+    my_id, err = api.join_game(server='127.0.0.1', port=4711, game='TicTacToe', token='mygame')
+
+    if err:
+        print(err)
+        exit()
+
+    print('Player ID:', my_id)
+
+
+t1 = threading.Thread(target=client_1, args=(), daemon=True)
+t1.start()
+t1.join()
+

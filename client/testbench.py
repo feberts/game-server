15a7018d487075921125bc261c78a4739b2dd40d
feberts <64541491+feberts@users.noreply.github.com>
2024-10-27 17:23:39 +0100

    Adding test bench (2)

---
+++ client/testbench.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Test bench for client server interaction in multiple threads.
+"""
+
+import threading
+import time
+from game_server_api import GameServerAPI
+
+game = 'TicTacToe'
+token = 'mygame'
+players = 2
+
+def client_start():
+    api = GameServerAPI()
+    my_id, err = api.start_game(server='127.0.0.1', port=4711, game=game, token=token, players=players)
+
+    if err:
+        print(err)
+        exit()
+
+    print('Player ID:', my_id)
+
+def client_join():
+    api = GameServerAPI()
+    my_id, err = api.join_game(server='127.0.0.1', port=4711, game=game, token=token)
+
+    if err:
+        print(err)
+        exit()
+
+    print(f'Player ID:', my_id)
+
+
+threading.Thread(target=client_start, args=(), daemon=True).start()
+
+time.sleep(0.5)
+
+for _ in range(players):
+    threading.Thread(target=client_join, args=(), daemon=True).start()
+
+time.sleep(5)

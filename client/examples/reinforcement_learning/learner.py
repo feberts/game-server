67e2b2818ac31f689bf9cb51419c26a4826dec73
feberts <64541491+feberts@users.noreply.github.com>
2024-11-23 00:56:27 +0100

    Streamline RL module

--- client/examples/reinforcement_learning/learner.py
+++ client/examples/reinforcement_learning/learner.py
@@ -2,7 +2,7 @@
 """
 Tic-tac-toe learner.
 
-This client learns how to play tic-tac-toe. It uses a method designed by Donald Michie in 1961 to develop a strategy. During training, a statistic is printed showing how the performance improves over time.
+This client learns how to play tic-tac-toe. It uses a method designed by British researcher Donald Michie in 1961 to develop a strategy. During training, a statistic is printed showing how the performance improves over time.
 
 Article by Donald Michie describing his method: https://academic.oup.com/comjnl/article/6/3/232/360077
 
@@ -14,54 +14,59 @@ import time
 
 from game_server_api import GameServerAPI
 
-# TODO aufräumen
-
 class MENACE:
     """
     Implementation of Donald Michie's Matchbox Educable Noughts and Crosses Engine (MENACE).
     """
 
     def __init__(self):
-        self.boxes = {} # board layout -> list of possible positions
-        self.current_game = {} # board layout -> chosen position
-        self.stage = 0 # stage in the current game
-        self.n_beads = [4, 3, 2, 1, 1] # initial number of beads of the same colour per game stage
+        self._boxes = {} # board layout -> list of empty positions
+        self._current_game = {} # board layout -> chosen position
+        self._stage = 0 # stage in the current game
+        self._number_of_beads = [4, 3, 2, 1, 1] # initial beads of each colour per stage
 
     def move(self, board):
         layout = tuple(board)
-        if layout not in self.boxes:
-            vacant = [i for i in range(9) if layout[i] == -1]
-            self.boxes[layout] = vacant * self.n_beads[self.stage]
-        pos = random.choice(self.boxes[layout])
-        self.current_game[layout] = pos
-        self.stage += 1
+
+        if layout not in self._boxes:
+            # initialize box with coloured beads (in the US, use colored beads):
+            coloured_beads = [i for i in range(9) if layout[i] == -1] # vacant positions
+            self._boxes[layout] = coloured_beads * self._number_of_beads[self._stage]
+
+        pos = random.choice(self._boxes[layout])
+        self._current_game[layout] = pos # used for backpropagation
+        self._stage += 1
+
         return pos
 
     def win(self):
         # backpropagation:
-        for layout, pos in self.current_game.items():
-            self.boxes[layout].append(pos) # old TODO
-            #self.boxes[layout].extend([pos] * 3) # new TODO
-        self.new_game()
+        for layout, pos in self._current_game.items():
+            self._boxes[layout].append(pos) # old TODO
+            #self._boxes[layout].extend([pos] * 3) # new TODO
 
-    def loose(self):
+        self._new_game()
+
+    def loss(self):
         # backpropagation:
-        for layout, pos in self.current_game.items():
-            if len(self.boxes[layout]) > 1: # keep last remaining bead
-                self.boxes[layout].remove(pos)
+        for layout, pos in self._current_game.items():
+            if len(self._boxes[layout]) > 1: # keep last remaining bead
+                self._boxes[layout].remove(pos)
             #else:
-                #del self.boxes[layout] # TODO works better?
-        self.new_game()
+                #del self._boxes[layout] # TODO works better?
+
+        self._new_game()
 
     def draw(self):
         # backpropagation:
-        #for layout, pos in self.current_game.items(): # new TODO
-            #self.boxes[layout].extend([pos] * 1) # new TODO
-        self.new_game()
+        #for layout, pos in self._current_game.items(): # new TODO
+            #self._boxes[layout].extend([pos] * 1) # new TODO
 
-    def new_game(self):
-        self.current_game = {}
-        self.stage = 0
+        self._new_game()
+
+    def _new_game(self):
+        self._current_game = {}
+        self._stage = 0
 
 def fatal(msg):
     print(msg)
@@ -71,23 +76,26 @@ class Statistic:
     def __init__(self):
         self.games = 0
         self.won = 0
-        self.lost = 0
         self.draw = 0
     def show(self):
         print(f'win: {self.won / self.games:.3f}',
               f'draw: {self.draw / self.games:.3f}')
 
 game = GameServerAPI()
+
 my_id, err = game.start_game(server='127.0.0.1', port=4711, game='TicTacToe', token='mygame', players=2)
 if err: fatal(err)
 
 menace = MENACE()
-stat = Statistic()
-reinforcements = 0
+statistic = Statistic()
+sample_size = 1000
+samples_max = 10
+samples = 0
+
 start = time.time()
-n = 0
-while n < 1000:
-    n += 1
+
+while samples < 1000:
+    samples += 1
     state, err = game.state()
     if err: fatal(err)
 
@@ -104,25 +112,19 @@ while n < 1000:
 
     if winner == my_id:
         menace.win()
-        stat.won += 1
-        reinforcements += 1
+        statistic.won += 1
     elif winner == None:
         menace.draw()
-        stat.draw += 1
-        reinforcements += 0
+        statistic.draw += 1
     else:
-        menace.loose()
-        stat.lost += 1
-        reinforcements -= 1
+        menace.loss()
 
     game.reset_game()
 
-    stat.games += 1
-    if stat.games == 1000:
-        stat.show()
-        stat = Statistic()
-    #print(stat.games, reinforcements, sep=',')
+    statistic.games += 1
 
-#print(''.join(outcome))
-#print(menace.current_game)
+    if statistic.games == sample_size:
+        statistic.show()
+        statistic = Statistic()
+
 print(time.time() - start)

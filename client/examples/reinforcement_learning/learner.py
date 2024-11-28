495715a7e7846dac5b90fadf46a3d37ef609405a
feberts <64541491+feberts@users.noreply.github.com>
2024-11-28 23:21:14 +0100

    Check and remove todos

--- client/examples/reinforcement_learning/learner.py
+++ client/examples/reinforcement_learning/learner.py
@@ -13,63 +13,10 @@ import random
 import time
 
 from game_server_api import GameServerAPI
+from menace import MENACE
 
 batch_size = 1000 # learning progress will be printed after each batch of games
 number_of_batches = 100
-
-class MENACE: # TODO in modul auslagern
-    """
-    Implementation of Donald Michie's Matchbox Educable Noughts and Crosses Engine (MENACE).
-    """
-
-    def __init__(self):
-        self._boxes = {} # board layout -> list of empty positions
-        self._current_game = {} # board layout -> chosen position
-        self._stage = 0 # stage in the current game
-        self._number_of_beads = [4, 3, 2, 1, 1] # initial beads of each colour per stage
-
-    def move(self, board):
-        layout = tuple(board)
-
-        if layout not in self._boxes:
-            # initialize box with coloured beads (in the US, use colored beads):
-            coloured_beads = [i for i in range(9) if layout[i] == -1] # vacant positions
-            self._boxes[layout] = coloured_beads * self._number_of_beads[self._stage]
-
-        pos = random.choice(self._boxes[layout])
-        self._current_game[layout] = pos # used for backpropagation
-        self._stage += 1
-
-        return pos
-
-    def win(self):
-        # backpropagation:
-        for layout, pos in self._current_game.items():
-            self._boxes[layout].append(pos) # add one bead to box
-            # self._boxes[layout].extend([pos] * 3) # add three beads to box
-
-        self._new_game()
-
-    def loss(self):
-        # backpropagation:
-        for layout, pos in self._current_game.items():
-            if len(self._boxes[layout]) > 1:
-                self._boxes[layout].remove(pos) # remove one bead from box
-            else:
-                del self._boxes[layout] # reset box after last bead is removed
-
-        self._new_game()
-
-    def draw(self):
-        # backpropagation:
-        # for layout, pos in self._current_game.items():
-            # self._boxes[layout].append(pos) # add one bead to box
-
-        self._new_game()
-
-    def _new_game(self):
-        self._current_game = {}
-        self._stage = 0
 
 def fatal(msg):
     print(msg)

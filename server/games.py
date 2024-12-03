28077d25eac98ef2bc7e06979b2f8f67f05413ce
feberts <64541491+feberts@users.noreply.github.com>
2024-12-03 23:47:11 +0100

    Add Yahtzee to list of games

--- server/games.py
+++ server/games.py
@@ -5,5 +5,6 @@ Every new game must be added to this list.
 """
 
 from tictactoe import TicTacToe
+from yahtzee import Yahtzee
 
-available_games = [TicTacToe]
+available_games = [TicTacToe, Yahtzee]

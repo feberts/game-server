7f3e737a47b395d810bb072ac5b21a89ff73148c
Fabian Eberts <64541491+feberts@users.noreply.github.com>
2025-12-07 18:04:32 +0100

    Change server config defaults

--- server/config.py
+++ server/config.py
@@ -9,19 +9,18 @@ ip = '127.0.0.1'
 port = 4711
 
 # FRAMEWORK:
-game_timeout = 180 # seconds, timeout for inactive games and for joining a game
+game_timeout = 300 # seconds, timeout for inactive games and for joining a game
 
 # LOGGING:
-log_server_info = False # useful only for debugging tcp connections (verbose)
+log_server_info = False # useful for debugging tcp connections (verbose)
 log_server_errors = True # errors during tcp connections
 log_framework_info = True # actions performed by the framework
 log_framework_request = True # client requests
 log_framework_response = True # server responses
 
 # TCP CONNECTIONS:
+# pick a higher value for request_size_max if required by a new game;
+# it should not be necessary to change buffer_size or connection_timeout
 request_size_max = int(1e6) # bytes, prevents clients from sending too much data
 buffer_size = 4096 # bytes, corresponds to client-side buffer size value
-connection_timeout = 60 # seconds, timeout for tcp connections
-
-# it should not be necessary to change buffer_size or connection_timeout;
-# pick a higher value for request_size_max if required by a new game
+connection_timeout = 60 # seconds, timeout for tcp transactions
